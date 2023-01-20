12bd63c15ca640c9e57d0343bdd4dcf8f29cb127
AVAtarMod <Respawner289@gmail.com>
2023-01-20 17:55:57 +0300

    (architecture) изменено: скрипты БД компонентов

--- architecture/first-level/component-hw-support/HW.db-script.sql
+++ architecture/first-level/component-hw-support/HW.db-script.sql
@@ -6,8 +6,8 @@ CREATE TABLE CPUInfo
  product_name varchar(120) NOT NULL,
  frequency    bigint NOT NULL,
  core_count   int NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( product_id ),
- CONSTRAINT FK_5 FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
+ CONSTRAINT PRIMARY KEY ( product_id ),
+ CONSTRAINT FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
 );
 
 CREATE INDEX FK_1 ON CPUInfo
@@ -21,8 +21,8 @@ CREATE TABLE Driver
  name        varchar(120) NOT NULL,
  description varchar(220) NOT NULL,
  is_active   boolean NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( hardware_id ),
- CONSTRAINT FK_8 FOREIGN KEY ( hardware_id ) REFERENCES HardwareItem ( hardware_id )
+ CONSTRAINT PRIMARY KEY ( hardware_id ),
+ CONSTRAINT FOREIGN KEY ( hardware_id ) REFERENCES HardwareItem ( hardware_id )
 );
 
 CREATE INDEX FK_1 ON Driver
@@ -39,8 +39,8 @@ CREATE TABLE GPUInfo
  frequency    bigint NOT NULL,
  core_count   int NOT NULL,
  memory_size  bigint NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( product_id ),
- CONSTRAINT FK_6 FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
+ CONSTRAINT PRIMARY KEY ( product_id ),
+ CONSTRAINT FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
 );
 
 CREATE INDEX FK_1 ON GPUInfo
@@ -53,7 +53,7 @@ CREATE TABLE HardwareInfo
 (
  hardware_info_id bigserial NOT NULL,
  device_model     varchar(100) NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( hardware_info_id )
+ CONSTRAINT PRIMARY KEY ( hardware_info_id )
 );
 
 
@@ -64,10 +64,10 @@ CREATE TABLE HardwareItem
  product_id       bigint NOT NULL,
  hardware_info_id bigint NOT NULL,
  class_id         int NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( hardware_id ),
- CONSTRAINT FK_1 FOREIGN KEY ( class_id ) REFERENCES HardwareItemClass ( class_id ),
- CONSTRAINT FK_3 FOREIGN KEY ( hardware_info_id ) REFERENCES HardwareInfo ( hardware_info_id ),
- CONSTRAINT FK_3_1 FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
+ CONSTRAINT PRIMARY KEY ( hardware_id ),
+ CONSTRAINT FOREIGN KEY ( class_id ) REFERENCES HardwareItemClass ( class_id ),
+ CONSTRAINT FOREIGN KEY ( hardware_info_id ) REFERENCES HardwareInfo ( hardware_info_id ),
+ CONSTRAINT FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
 );
 
 CREATE INDEX FK_2 ON HardwareItem
@@ -80,7 +80,7 @@ CREATE TABLE HardwareItemClass
  class_id    serial NOT NULL,
  name        varchar(60) NOT NULL,
  description varchar(220) NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( class_id )
+ CONSTRAINT PRIMARY KEY ( class_id )
 );
 
 CREATE TABLE MemoryInfo
@@ -92,8 +92,8 @@ CREATE TABLE MemoryInfo
  capacity     bigint NOT NULL,
  technology   int NOT NULL,
  speed        bigint NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( product_id ),
- CONSTRAINT FK_4 FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
+ CONSTRAINT PRIMARY KEY ( product_id ),
+ CONSTRAINT FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
 );
 
 CREATE INDEX FK_2 ON MemoryInfo
@@ -104,7 +104,7 @@ CREATE INDEX FK_2 ON MemoryInfo
 CREATE TABLE Product
 (
  product_id bigserial NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( product_id )
+ CONSTRAINT PRIMARY KEY ( product_id )
 );
 
 CREATE TABLE StorageInfo
@@ -115,8 +115,8 @@ CREATE TABLE StorageInfo
  product_name varchar(120) NOT NULL,
  capacity     bigint NOT NULL,
  type         int NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( product_id ),
- CONSTRAINT FK_7 FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
+ CONSTRAINT PRIMARY KEY ( product_id ),
+ CONSTRAINT FOREIGN KEY ( product_id ) REFERENCES Product ( product_id )
 );
 
 CREATE INDEX FK_1 ON StorageInfo
--- architecture/first-level/component-storage/CSt.db-script.sql
+++ architecture/first-level/component-storage/CSt.db-script.sql
@@ -1,113 +1,120 @@
-CREATE TABLE "Storage@EcosystemTeam".Ecosystem
-(
- ecosystem_id      serial NOT NULL,
- storage_policy_id bigint NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( ecosystem_id ),
- CONSTRAINT FK_1 FOREIGN KEY ( storage_policy_id ) REFERENCES "Storage@EcosystemTeam".StoragePolicy ( storage_policy_id )
-);
-CREATE INDEX FK_2 ON "Storage@EcosystemTeam".Ecosystem
-(
- storage_policy_id
-);
-
-
-CREATE TABLE "Storage@EcosystemTeam".Component
+CREATE TABLE Component
 (
  component_id bigserial NOT NULL,
  ui_logo           varchar(50) NULL,
  time_install      timestamp NOT NULL,
  time_update       timestamp NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( component_id )
-);
-CREATE TABLE "Storage@EcosystemTeam"."Ecosystem-Component"
-(
- component_id bigint NOT NULL,
- ecosystem_id      int NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( component_id, ecosystem_id ),
- CONSTRAINT FK_6_1 FOREIGN KEY ( component_id ) REFERENCES "Storage@EcosystemTeam".Component ( component_id ),
- CONSTRAINT FK_7 FOREIGN KEY ( ecosystem_id ) REFERENCES "Storage@EcosystemTeam".Ecosystem ( ecosystem_id )
-);
-CREATE INDEX FK_1 ON "Storage@EcosystemTeam"."Ecosystem-Component"
-(
- component_id
-);
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam"."Ecosystem-Component"
-(
- ecosystem_id
+ PRIMARY KEY ( component_id )
 );
 
-
-CREATE TABLE "Storage@EcosystemTeam".StoragePolicy
-(
- storage_policy_id           bigserial NOT NULL,
- use_shared_views            boolean NOT NULL,
- space_restriction_policy_id bigint NOT NULL,
- create_time                 timestamp NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( storage_policy_id ),
- CONSTRAINT FK_2 FOREIGN KEY ( space_restriction_policy_id ) REFERENCES "Storage@EcosystemTeam".SpaceRestrictionPolicy ( space_restriction_policy_id )
-);
-CREATE INDEX FK_2 ON "Storage@EcosystemTeam".StoragePolicy
-(
- space_restriction_policy_id
-);
-
-
-CREATE TABLE "Storage@EcosystemTeam".SpaceRestrictionPolicy
+CREATE TABLE SpaceRestrictionPolicy
 (
  space_restriction_policy_id bigserial NOT NULL,
  ui_name                     varchar(50) NOT NULL,
  create_time                 timestamp NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( space_restriction_policy_id )
+ PRIMARY KEY ( space_restriction_policy_id )
 );
 
+CREATE TABLE ComponentRestriction
+(
+ parent_policy_id  bigint NOT NULL,
+ component_id bigint NOT NULL,
+ max_size          bigint NOT NULL,
+ PRIMARY KEY ( parent_policy_id, component_id ),
+ FOREIGN KEY ( parent_policy_id ) REFERENCES SpaceRestrictionPolicy ( space_restriction_policy_id ),
+ FOREIGN KEY ( component_id ) REFERENCES Component ( component_id )
+);
 
-CREATE TABLE "Storage@EcosystemTeam".UserRestriction
+CREATE TABLE ExternalView_User
+(
+ user_id bigserial NOT NULL,
+ PRIMARY KEY ( user_id )
+);
+
+CREATE TABLE UserRestriction
 (
  parent_policy_id      bigint NOT NULL,
  user_id               bigint NOT NULL,
  max_size              bigint NOT NULL,
  is_allow_partitioning boolean NOT NULL,
  max_partition_count   bigint NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( parent_policy_id, user_id ),
- CONSTRAINT FK_3 FOREIGN KEY ( parent_policy_id ) REFERENCES "Storage@EcosystemTeam".SpaceRestrictionPolicy ( space_restriction_policy_id ),
- CONSTRAINT FK_6 FOREIGN KEY ( user_id ) REFERENCES ExternalView_User ( user_id )
+ PRIMARY KEY ( parent_policy_id, user_id ),
+ FOREIGN KEY ( parent_policy_id ) REFERENCES SpaceRestrictionPolicy ( space_restriction_policy_id ),
+ FOREIGN KEY ( user_id ) REFERENCES ExternalView_User ( user_id )
 );
-CREATE INDEX FK_1 ON "Storage@EcosystemTeam".UserRestriction
+CREATE INDEX INDEX_1 ON UserRestriction
 (
  parent_policy_id
 );
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam".UserRestriction
+CREATE INDEX INDEX_2 ON UserRestriction
 (
  user_id
 );
-CREATE TABLE ExternalView_User
+
+CREATE TABLE StoragePolicy
 (
- user_id bigserial NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( user_id )
+ storage_policy_id           bigserial NOT NULL,
+ use_shared_views            boolean NOT NULL,
+ space_restriction_policy_id bigint NOT NULL,
+ create_time                 timestamp NOT NULL,
+ PRIMARY KEY ( storage_policy_id ),
+ FOREIGN KEY ( space_restriction_policy_id ) REFERENCES SpaceRestrictionPolicy ( space_restriction_policy_id )
+);
+CREATE INDEX INDEX_3 ON StoragePolicy
+(
+ space_restriction_policy_id
 );
 
-
-CREATE TABLE "Storage@EcosystemTeam".ComponentRestriction
+CREATE TABLE Ecosystem
 (
- parent_policy_id  bigint NOT NULL,
+ ecosystem_id      serial NOT NULL,
+ storage_policy_id bigint NOT NULL,
+ PRIMARY KEY ( ecosystem_id ),
+ FOREIGN KEY ( storage_policy_id ) REFERENCES StoragePolicy ( storage_policy_id )
+);
+CREATE INDEX INDEX_4 ON Ecosystem
+(
+ storage_policy_id
+);
+
+CREATE TABLE "Ecosystem-Component"
+(
  component_id bigint NOT NULL,
- max_size          bigint NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( parent_policy_id, component_id ),
- CONSTRAINT FK_4 FOREIGN KEY ( parent_policy_id ) REFERENCES "Storage@EcosystemTeam".SpaceRestrictionPolicy ( space_restriction_policy_id ),
- CONSTRAINT FK_8 FOREIGN KEY ( component_id ) REFERENCES "Storage@EcosystemTeam".Component ( component_id )
+ ecosystem_id      int NOT NULL,
+ PRIMARY KEY ( component_id, ecosystem_id ),
+ FOREIGN KEY ( component_id ) REFERENCES Component ( component_id ),
+ FOREIGN KEY ( ecosystem_id ) REFERENCES Ecosystem ( ecosystem_id )
 );
-CREATE INDEX FK_2 ON "Storage@EcosystemTeam".ComponentRestriction
+CREATE INDEX INDEX_5 ON "Ecosystem-Component"
+(
+ component_id
+);
+CREATE INDEX INDEX_6 ON "Ecosystem-Component"
+(
+ ecosystem_id
+);
+
+
+CREATE INDEX INDEX_7 ON ComponentRestriction
 (
  parent_policy_id
 );
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam".ComponentRestriction
+CREATE INDEX INDEX_8 ON ComponentRestriction
 (
  component_id
 );
 
 
 -- Manifest
-CREATE TABLE "Storage@EcosystemTeam".Manifest
+
+CREATE TABLE ManifestScheme
+(
+ scheme_id         bigint NOT NULL,
+ field_description jsonb NOT NULL,
+ PRIMARY KEY ( scheme_id )
+);
+
+CREATE TABLE Manifest
 (
  manifest_id    bigserial NOT NULL,
  hash           varchar(512) NOT NULL,
@@ -116,149 +123,142 @@ CREATE TABLE "Storage@EcosystemTeam".Manifest
  ui_name        varchar(80) NOT NULL,
  ui_description varchar(350) NOT NULL,
  version        varchar(50) NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( manifest_id ),
- CONSTRAINT FK_11 FOREIGN KEY ( scheme_id ) REFERENCES "Storage@EcosystemTeam".ManifestScheme ( scheme_id )
+ PRIMARY KEY ( manifest_id ),
+ FOREIGN KEY ( scheme_id ) REFERENCES ManifestScheme ( scheme_id )
 );
-CREATE TABLE "Storage@EcosystemTeam"."Component-Manifest"
+CREATE TABLE "Component-Manifest"
 (
- component_info_id bigint NOT NULL,
+ component_id bigint NOT NULL,
  manifest_id       bigint NOT NULL,
- CONSTRAINT PK_2 PRIMARY KEY ( component_info_id, manifest_id ),
- CONSTRAINT FK_9 FOREIGN KEY ( component_info_id ) REFERENCES "Storage@EcosystemTeam".ComponentInfo ( component_info_id ),
- CONSTRAINT FK_10 FOREIGN KEY ( manifest_id ) REFERENCES "Storage@EcosystemTeam".Manifest ( manifest_id )
+ PRIMARY KEY ( component_id, manifest_id ),
+ FOREIGN KEY ( component_id ) REFERENCES Component ( component_id ),
+ FOREIGN KEY ( manifest_id ) REFERENCES Manifest ( manifest_id )
 );
-CREATE INDEX FK_1 ON "Storage@EcosystemTeam"."Component-Manifest"
+CREATE INDEX INDEX_9 ON "Component-Manifest"
 (
- component_info_id
+ component_id
 );
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam"."Component-Manifest"
+CREATE INDEX INDEX_10 ON "Component-Manifest"
 (
  manifest_id
 );
 
-CREATE INDEX FK_2 ON "Storage@EcosystemTeam".Manifest
+CREATE INDEX INDEX_11 ON Manifest
 (
  scheme_id
 );
 
 
-CREATE TABLE "Storage@EcosystemTeam".ManifestScheme
-(
- scheme_id         bigint NOT NULL,
- field_description jsonb NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( scheme_id )
-);
-
 --    Begin Manifest fields:
 --       NetworkAccess
-CREATE TABLE "Storage@EcosystemTeam".NetworkAccess
+CREATE TABLE NetworkAccess
 (
  net_access_id bigserial NOT NULL,
  "uri"           varchar(500) NOT NULL,
  protocol      varchar(50) NOT NULL,
  port          int NOT NULL,
  is_optional   boolean NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( net_access_id )
+ PRIMARY KEY ( net_access_id )
 );
 
-CREATE TABLE "Storage@EcosystemTeam"."NetworkAccess-Manifest"
-(
- field_id    bigint NOT NULL,
- manifest_id bigint NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( field_id, manifest_id ),
- CONSTRAINT FK_14 FOREIGN KEY ( field_id ) REFERENCES "Storage@EcosystemTeam".NetworkAccess ( net_access_id ),
- CONSTRAINT FK_15 FOREIGN KEY ( manifest_id ) REFERENCES "Storage@EcosystemTeam".Manifest ( manifest_id )
-);
+   CREATE TABLE "NetworkAccess-Manifest"
+   (
+   field_id    bigint NOT NULL,
+   manifest_id bigint NOT NULL,
+   PRIMARY KEY ( field_id, manifest_id ),
+   FOREIGN KEY ( field_id ) REFERENCES NetworkAccess ( net_access_id ),
+   FOREIGN KEY ( manifest_id ) REFERENCES Manifest ( manifest_id )
+   );
 
-CREATE INDEX FK_1 ON "Storage@EcosystemTeam"."NetworkAccess-Manifest"
+CREATE INDEX INDEX_12 ON "NetworkAccess-Manifest"
 (
  field_id
 );
 
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam"."NetworkAccess-Manifest"
+CREATE INDEX INDEX_13 ON "NetworkAccess-Manifest"
 (
  manifest_id
 );
 --       /NetworkAccess
 
 --       BaseInfo
-CREATE TABLE "Storage@EcosystemTeam".BaseInfo
+CREATE TABLE BaseInfo
 (
  base_info_id   bigserial NOT NULL,
  min_sdk_ver    int NOT NULL,
  target_sdk_ver int NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( base_info_id )
+ PRIMARY KEY ( base_info_id )
 );
 
-CREATE TABLE "Storage@EcosystemTeam"."BaseInfo-Manifest"
+CREATE TABLE "BaseInfo-Manifest"
 (
  base_info_id bigint NOT NULL,
  manifest_id  bigint NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( base_info_id, manifest_id ),
- CONSTRAINT FK_18 FOREIGN KEY ( base_info_id ) REFERENCES "Storage@EcosystemTeam".BaseInfo ( base_info_id ),
- CONSTRAINT FK_19 FOREIGN KEY ( manifest_id ) REFERENCES "Storage@EcosystemTeam".Manifest ( manifest_id )
+ PRIMARY KEY ( base_info_id, manifest_id ),
+ FOREIGN KEY ( base_info_id ) REFERENCES BaseInfo ( base_info_id ),
+ FOREIGN KEY ( manifest_id ) REFERENCES Manifest ( manifest_id )
 );
-CREATE INDEX FK_1 ON "Storage@EcosystemTeam"."BaseInfo-Manifest"
+CREATE INDEX INDEX_14 ON "BaseInfo-Manifest"
 (
  base_info_id
 );
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam"."BaseInfo-Manifest"
+CREATE INDEX INDEX_15 ON "BaseInfo-Manifest"
 (
  manifest_id
 );
 -- 		/BaseInfo
 
 -- 		ComponentPartInfo
-CREATE TABLE "Storage@EcosystemTeam".ComponentPartInfo
+CREATE TABLE ComponentPartInfo
 (
  component_part_info_id bigserial NOT NULL,
  type                   int NOT NULL,
  priority               int NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( component_part_info_id )
+ PRIMARY KEY ( component_part_info_id )
 );
 
-CREATE TABLE "Storage@EcosystemTeam"."ComponentPartInfo-Manifest"
+CREATE TABLE "ComponentPartInfo-Manifest"
 (
  manifest_id bigint NOT NULL,
  field_id    bigint NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( manifest_id, field_id ),
- CONSTRAINT FK_12 FOREIGN KEY ( manifest_id ) REFERENCES "Storage@EcosystemTeam".Manifest ( manifest_id ),
- CONSTRAINT FK_13 FOREIGN KEY ( field_id ) REFERENCES "Storage@EcosystemTeam".ComponentPartInfo ( component_part_info_id )
+ PRIMARY KEY ( manifest_id, field_id ),
+ FOREIGN KEY ( manifest_id ) REFERENCES Manifest ( manifest_id ),
+ FOREIGN KEY ( field_id ) REFERENCES ComponentPartInfo ( component_part_info_id )
 );
-CREATE INDEX FK_2 ON "Storage@EcosystemTeam"."ComponentPartInfo-Manifest"
+CREATE INDEX INDEX_16 ON "ComponentPartInfo-Manifest"
 (
  manifest_id
 );
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam"."ComponentPartInfo-Manifest"
+CREATE INDEX INDEX_17 ON "ComponentPartInfo-Manifest"
 (
  field_id
 );
 -- 		/ComponentPartInfo
 
 -- 		FilesystemAccess
-CREATE TABLE "Storage@EcosystemTeam".FilesystemAccess
+CREATE TABLE FilesystemAccess
 (
  fs_access_id bigserial NOT NULL,
  "path"         varchar(500) NOT NULL,
  permissions  char(10) NOT NULL,
  type         int NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( fs_access_id )
+ PRIMARY KEY ( fs_access_id )
 );
 
 
-CREATE TABLE "Storage@EcosystemTeam"."FilesystemAccess-Manifest"
+CREATE TABLE "FilesystemAccess-Manifest"
 (
  fs_access_id bigint NOT NULL,
  manifest_id  bigint NOT NULL,
- CONSTRAINT PK_1 PRIMARY KEY ( fs_access_id, manifest_id ),
- CONSTRAINT FK_16 FOREIGN KEY ( fs_access_id ) REFERENCES "Storage@EcosystemTeam".FilesystemAccess ( fs_access_id ),
- CONSTRAINT FK_17 FOREIGN KEY ( manifest_id ) REFERENCES "Storage@EcosystemTeam".Manifest ( manifest_id )
+ PRIMARY KEY ( fs_access_id, manifest_id ),
+ FOREIGN KEY ( fs_access_id ) REFERENCES FilesystemAccess ( fs_access_id ),
+ FOREIGN KEY ( manifest_id ) REFERENCES Manifest ( manifest_id )
 );
-CREATE INDEX FK_2 ON "Storage@EcosystemTeam"."FilesystemAccess-Manifest"
+CREATE INDEX INDEX_18 ON "FilesystemAccess-Manifest"
 (
  fs_access_id
 );
-CREATE INDEX FK_3 ON "Storage@EcosystemTeam"."FilesystemAccess-Manifest"
+CREATE INDEX INDEX_19 ON "FilesystemAccess-Manifest"
 (
  manifest_id
 );
