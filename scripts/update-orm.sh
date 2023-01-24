aec4d3726291f64de2ae8465a24d13c8aa3b5cb3
AVAtarMod <Respawner289@gmail.com>
2023-01-24 17:01:36 +0300

    (code) обновлено: шаблон, скрипты

--- scripts/update-orm.sh
+++ scripts/update-orm.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
-sqlpp11-ddl2cpp architecture/first-level/component-storage/CSt.db-script.sql code/component-storage/orm model
-sqlpp11-ddl2cpp architecture/first-level/tool-CM/TCM.db-script.sql code/tool-CM/orm model
-sqlpp11-ddl2cpp architecture/first-level/component-hw-support/HW.db-script.sql code/component-hw-support/orm model
+sqlpp11-ddl2cpp architecture/first-level/component-storage/CSt.db-script.sql code/component-storage/storage/orm model
+sqlpp11-ddl2cpp architecture/first-level/tool-CM/TCM.db-script.sql code/tool-CM/storage/orm model
+sqlpp11-ddl2cpp architecture/first-level/component-hw-support/HW.db-script.sql code/component-hw-support/storage/orm model
